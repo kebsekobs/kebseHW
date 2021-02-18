0ae84b346eecbfa77f5c9df972652c486288a79b
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-02-18 21:40:56 +0300

    Sdelal testy

---
+++ createArray/HW1/test/TestDynamicArray.java
@@ -0,0 +1,40 @@
+package HW1.test;
+
+import HW1.code.DynamicArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestDynamicArray extends Assertions {
+    @Test
+    public void test_resize_and_size(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.resize(1);
+        assertEquals(1, dynamicArray.size());
+    }
+    @Test
+    public void test_set_and_get(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.resize(1);
+        dynamicArray.set(0, 5);
+        assertEquals(5, dynamicArray.get(0));
+    }
+    @Test
+    public void test_resize_exception(){
+        DynamicArray dynamicArray = new DynamicArray();
+        assertThrows(ArithmeticException.class, () -> dynamicArray.resize(-1));
+    }
+
+    @Test
+    public void test_get_exception(){
+        DynamicArray dynamicArray = new DynamicArray();
+        assertThrows(ArithmeticException.class, () -> dynamicArray.get(-1));
+        assertThrows(ArithmeticException.class, () -> dynamicArray.get(100));
+    }
+
+    @Test
+    public void test_set_exception(){
+        DynamicArray dynamicArray = new DynamicArray();
+        assertThrows(ArithmeticException.class, () -> dynamicArray.set(-1, 5));
+        assertThrows(ArithmeticException.class, () -> dynamicArray.set(100, 18));
+    }
+}

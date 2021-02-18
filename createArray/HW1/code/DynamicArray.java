dd8c848c4ee3f26ce4b698951722438ad238ab7b
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-02-18 21:40:38 +0300

    Zakonchil prikoly s programmoy

---
+++ createArray/HW1/code/DynamicArray.java
@@ -0,0 +1,38 @@
+package HW1.code;
+
+public class DynamicArray <T>{
+    private final int DEFAULT_SIZE = 10;
+    private T[] array;
+
+    public DynamicArray(){
+        array = (T[])new Object[DEFAULT_SIZE];
+    }
+
+    public T get(int index){
+        if(size() < index | index < 0){throwException();}
+        return array[index];
+    }
+
+    public void set(int index,T value){
+        if(size() < index | index < 0){throwException();}
+        array[index] = value;
+    }
+
+    public void resize(int newSize){
+        if (newSize < 0){throwException();}
+        T[] newArray = array.clone();
+        array = (T[])new Object[newSize];
+        for(int i = 0; i < newSize; i++){
+            array[i] = newArray[i];
+        }
+    }
+
+    public int size(){
+        return array.length;
+    }
+
+    public void throwException(){
+        throw new ArithmeticException("ЭЭээээээ куда прёшь??!?!?!??!?");
+    }
+}
+

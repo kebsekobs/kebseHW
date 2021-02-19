90a4b82c232c30d25f9bd397a115ce959371be9d
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-02-19 11:28:19 +0300

    test findFirst

--- createArray/HW1/test/TestDynamicArray.java
+++ createArray/HW1/test/TestDynamicArray.java
@@ -49,4 +49,24 @@ public class TestDynamicArray extends Assertions {
         DynamicArray dynamicArray = new DynamicArray(-5);
         assertEquals(10, dynamicArray.size());
     }
+
+    @Test
+    public void test_findFirst(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.resize(3);
+        dynamicArray.set(0, 5);
+        dynamicArray.set(1, 2);
+        dynamicArray.set(2, 5);
+        assertEquals(0, dynamicArray.findFirst(5));
+    }
+
+    @Test
+    public void test_findFirst_incorrect(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.resize(3);
+        dynamicArray.set(0, 5);
+        dynamicArray.set(1, 2);
+        dynamicArray.set(2, 5);
+        assertEquals(-1, dynamicArray.findFirst(3));
+    }
 }

b821760cb85ff0c48be4a5656b8653becf8b9ab9
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-04 23:25:06 +0300

    fix some troubles + create search and insert

--- LinkedList/src/HW2/code/DoubleLinkedList.java
+++ LinkedList/src/HW2/code/DoubleLinkedList.java
@@ -1,8 +1,8 @@
 package HW2.code;
 
 public class DoubleLinkedList<T>{
-    private DoubleLinkedListElement first;
-    private DoubleLinkedListElement last;
+    private DoubleLinkedListElement<T> first;
+    private DoubleLinkedListElement<T> last;
 
     public DoubleLinkedList() {
         first =  null;
@@ -13,7 +13,7 @@ public class DoubleLinkedList<T>{
         final DoubleLinkedListElement currFirst = first;
         final DoubleLinkedListElement newDoubleLinkedListElement = new DoubleLinkedListElement(null, currFirst, Value);
         first = newDoubleLinkedListElement;
-        if(currFirst == null)  // if empty list
+        if(currFirst == null)
             last = newDoubleLinkedListElement;
         else
             currFirst.prev = newDoubleLinkedListElement;
@@ -23,17 +23,37 @@ public class DoubleLinkedList<T>{
         final DoubleLinkedListElement currLast = last;
         final DoubleLinkedListElement newDoubleLinkedListElement = new DoubleLinkedListElement(currLast, null, Value);
         last = newDoubleLinkedListElement;
-        if(currLast == null)  // if empty list
+            if(currLast == null)
             first = newDoubleLinkedListElement;
         else
             currLast.next = newDoubleLinkedListElement;
     }
 
     public T getLast() {
-        return (T) last.value;
+        return last.value;
     }
 
     public T getFirst() {
-        return (T) first.value;
+        return first.value;
+    }
+
+    public void insert(DoubleLinkedListElement<T> element, T newElement) {
+        DoubleLinkedListElement<T> node = new DoubleLinkedListElement<T>(element, element.next, newElement);
+        node.next = element.next;
+        node.prev = element;
+        if (element == last) addLast(newElement);
+        else {
+            element.next = node;
+            node.next.prev = node;
+        }
+    }
+
+    public DoubleLinkedListElement<T> search(T searchValue) {
+        DoubleLinkedListElement<T> element = first;
+        while (element != null) {
+            if (element.value.equals(searchValue)) return element;
+            element = element.next;
+        }
+        return null;
     }
 }
--- LinkedList/src/HW2/code/DoubleLinkedListElement.java
+++ LinkedList/src/HW2/code/DoubleLinkedListElement.java
@@ -1,13 +1,13 @@
 package HW2.code;
 
 public class DoubleLinkedListElement <T> {
-    DoubleLinkedListElement next;
-    DoubleLinkedListElement prev;
+    DoubleLinkedListElement<T> next;
+    DoubleLinkedListElement<T> prev;
     T value;
 
     public DoubleLinkedListElement(DoubleLinkedListElement Prev, DoubleLinkedListElement Next, T Value) {
-        prev = Prev;
-        next = Next;
-        value = Value;
+        this.prev = Prev;
+        this.next = Next;
+        this.value = Value;
     }
 }

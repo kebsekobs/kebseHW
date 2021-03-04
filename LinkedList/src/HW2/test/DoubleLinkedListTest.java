de36c3aac5dcdb31ced18ae4903ad85e9d6b1164
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-04 23:25:24 +0300

    teststtststst

--- LinkedList/src/HW2/test/DoubleLinkedListTest.java
+++ LinkedList/src/HW2/test/DoubleLinkedListTest.java
@@ -7,13 +7,22 @@ import org.junit.jupiter.api.Test;
 public class DoubleLinkedListTest  extends Assertions {
 
     @Test
-    public void test_resize_and_size(){
+    public void test_add_first_and_last_and_get_first_and_last(){
         DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
         doubleLinkedList.addFirst(15);
         doubleLinkedList.addLast(21);
         assertEquals(15, doubleLinkedList.getFirst());
         assertEquals(21, doubleLinkedList.getLast());
-
-
     }
+
+    @Test
+    public void test_insert_and_search(){
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.addFirst(15);
+        doubleLinkedList.addLast(21);
+        doubleLinkedList.insert(doubleLinkedList.search(21), 26);
+        assertEquals(15, doubleLinkedList.getFirst());
+        assertEquals(26, doubleLinkedList.getLast());
+    }
+
 }

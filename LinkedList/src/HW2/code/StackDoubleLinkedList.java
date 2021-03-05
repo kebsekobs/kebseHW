6e789170a6ee586505003da5ab9c184d132f58e0
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-05 11:52:52 +0300

    V1.0 is ready

---
+++ LinkedList/src/HW2/code/StackDoubleLinkedList.java
@@ -0,0 +1,30 @@
+package HW2.code;
+
+public class StackDoubleLinkedList<T> {
+    private DoubleLinkedList<T> data;
+    private DoubleLinkedListElement<T> top;
+
+    public StackDoubleLinkedList() {
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        this.data = doubleLinkedList;
+        this.top = data.getHead();
+    }
+
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+
+    public void push(T element){
+        data.addFirst(element);
+    }
+
+    public T pop(){
+        T forReturn = data.getFirst();
+        data.deleteFirst();
+        return forReturn;
+    }
+
+    public T peek(){
+        return data.getFirst();
+    }
+}

53ae00b8c86279c110c6bc0d4a186a9a08a4a691
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-11 22:56:11 +0300

    final tests

--- LinkedList/src/HW2/test/StackDoubleLinkedListTest.java
+++ LinkedList/src/HW2/test/StackDoubleLinkedListTest.java
@@ -5,8 +5,38 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StackDoubleLinkedListTest extends Assertions{
+
     @Test
-    public void test_add_first_and_last_and_get_first_and_last(){
+    public void test_push(){
+        StackDoubleLinkedList stackDoubleLinkedList = new StackDoubleLinkedList();
+        stackDoubleLinkedList.push(15);
+        assertEquals(15, stackDoubleLinkedList.peek());
+    }
+
+    @Test
+    public void test_pop(){
+        StackDoubleLinkedList stackDoubleLinkedList = new StackDoubleLinkedList();
+        stackDoubleLinkedList.push(15);
+        assertEquals(15, stackDoubleLinkedList.peek());
+        stackDoubleLinkedList.push(21);
+        assertEquals(21, stackDoubleLinkedList.pop());
+        assertEquals(15, stackDoubleLinkedList.pop());
+    }
+
+
+    @Test
+    public void test_peek(){
+        StackDoubleLinkedList stackDoubleLinkedList = new StackDoubleLinkedList();
+        stackDoubleLinkedList.push(15);
+        assertEquals(15, stackDoubleLinkedList.peek());
+        stackDoubleLinkedList.push(21);
+        assertEquals(21, stackDoubleLinkedList.pop());
+        assertEquals(15, stackDoubleLinkedList.pop());
+    }
+
+
+    @Test
+    public void test_isEmpty(){
         StackDoubleLinkedList stackDoubleLinkedList = new StackDoubleLinkedList();
         stackDoubleLinkedList.push(15);
         assertEquals(15, stackDoubleLinkedList.peek());

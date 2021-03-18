c37c88cb9a849d010074617ec2368f270e069225
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-18 23:15:42 +0300

    Забыл сделать пару коммитов(увлекся работой)

--- createArray/HW1/code/QueueDynamicArray.java
+++ createArray/HW1/code/QueueDynamicArray.java
@@ -1,32 +1,57 @@
 package HW1.code;
 
-import javax.naming.SizeLimitExceededException;
-
 public class QueueDynamicArray<T> {
     private DynamicArray<T> data;
-    private int head = 0;
+    private int head;
     private int tail = 0;
 
     public QueueDynamicArray(int size){
         DynamicArray dynamicArray = new DynamicArray(size);
         this.data = dynamicArray;
+        this.head = size - 1;
     }
 
-    public void Enque(){
-        if(tail < data.capacity()){
-            if(data.get(tail) != null){
-                throwException();
+    public void enqueue(T value){
+            if(tail >= data.capacity()){
+                if(data.get(0) != null){
+                    throwMemException();
+                }
+                else {
+                    data.set(0, value);
+                    head = 0;
+                    tail = 1;
+                }
             }
+            else if(data.get(tail) != null){
+                throwMemException();
+            }
+            else {
+                data.set(tail, value);
+                head = tail;
+                tail += 1;
+            }
+    }
+
+    public void dequeue(){
+        if(data.get(head) != null){
+            data.set(head, null);
+            if(head == 0){ head = data.capacity() - 1;}
+
         }
+        else {throwNullException();}
     }
 
 
-    public void throwException() {
-        try {
-            throw new SizeLimitExceededException("Ну и что, что массив динамический, очередь же зациклена");
-        } catch (SizeLimitExceededException e) {
-            e.printStackTrace();
-        }
+
+
+    public void throwMemException(){
+        throw new ArithmeticException("Ну и что, что массив динамический, очередь же зациклена");
+        /*в java есть Checked и Unchecked исключения и мне очень нужно Unchecked, но их очень мало,
+        * а самому такое я не знаю как написать(знаю только как написать Checked), поэтому буду
+        * использовать везде арифметик*/
+    }
+    public void throwNullException(){
+        throw new NullPointerException("Пусто пусто выросла капуста");
     }
 }
 

7f15d14564c65d6f7de42c5efed731ae6a3b8a74
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-18 23:16:08 +0300

    Первые тесты

--- createArray/HW1/test/TestQueueDynamicArray.java
+++ createArray/HW1/test/TestQueueDynamicArray.java
@@ -1,5 +1,26 @@
 package HW1.test;
 
-public class TestQueueDynamicArray {
+import HW1.code.QueueDynamicArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.naming.SizeLimitExceededException;
+
+public class TestQueueDynamicArray extends Assertions{
+    @Test
+    public void test_init(){
+        QueueDynamicArray queueDynamicArray = new QueueDynamicArray(5);
+    }
+
+    @Test
+    public void test_enqueue(){
+        QueueDynamicArray queueDynamicArray = new QueueDynamicArray(5);
+        queueDynamicArray.enqueue(1);
+        queueDynamicArray.enqueue(2);
+        queueDynamicArray.enqueue(23);
+        queueDynamicArray.enqueue(25);
+        queueDynamicArray.enqueue(28);
+        assertThrows(ArithmeticException.class, () -> queueDynamicArray.enqueue(999));
+    }
 
 }

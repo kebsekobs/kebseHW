7f7f53568f8a130122049e62e8767bd0091423b0
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-03-19 00:11:41 +0300

    remake

--- createArray/HW1/test/TestQueueDynamicArray.java
+++ createArray/HW1/test/TestQueueDynamicArray.java
@@ -4,8 +4,6 @@ import HW1.code.QueueDynamicArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.naming.SizeLimitExceededException;
-
 public class TestQueueDynamicArray extends Assertions{
     @Test
     public void test_init(){
@@ -15,21 +13,21 @@ public class TestQueueDynamicArray extends Assertions{
     @Test
     public void test_enqueue(){
         QueueDynamicArray queueDynamicArray = new QueueDynamicArray(5);
-        queueDynamicArray.enqueue(1);
-        queueDynamicArray.enqueue(2);
-        queueDynamicArray.enqueue(23);
-        queueDynamicArray.enqueue(25);
-        queueDynamicArray.enqueue(28);
+        queueDynamicArray.enqueue('a');
+        queueDynamicArray.enqueue('b');
+        queueDynamicArray.enqueue('c');
+        queueDynamicArray.enqueue('d');
+        queueDynamicArray.enqueue('e');
         assertThrows(ArithmeticException.class, () -> queueDynamicArray.enqueue(999));
     }
-
+    @Test
     public void test_dequeue(){
         QueueDynamicArray queueDynamicArray = new QueueDynamicArray(5);
-        queueDynamicArray.enqueue(1);
-        queueDynamicArray.enqueue(2);
-        queueDynamicArray.enqueue(23);
-        queueDynamicArray.enqueue(25);
-        queueDynamicArray.enqueue(28);
+        queueDynamicArray.enqueue('a');
+        queueDynamicArray.enqueue('b');
+        queueDynamicArray.enqueue('c');
+        queueDynamicArray.enqueue('d');
+        queueDynamicArray.enqueue('e');
         queueDynamicArray.dequeue();
         queueDynamicArray.dequeue();
         queueDynamicArray.dequeue();
@@ -37,23 +35,46 @@ public class TestQueueDynamicArray extends Assertions{
         queueDynamicArray.dequeue();
         assertThrows(NullPointerException.class, () -> queueDynamicArray.dequeue());
     }
-
+    @Test
     public void test_peek() {
         QueueDynamicArray queueDynamicArray = new QueueDynamicArray(5);
 
         assertThrows(NullPointerException.class, () -> queueDynamicArray.peek());
-        queueDynamicArray.enqueue(1);
-        assertEquals(1, queueDynamicArray.peek());
-        queueDynamicArray.enqueue(2);
-        assertEquals(2, queueDynamicArray.peek());
-        queueDynamicArray.enqueue(23);
-        assertEquals(23, queueDynamicArray.peek());
-        queueDynamicArray.enqueue(25);
-        assertEquals(25, queueDynamicArray.peek());
-        queueDynamicArray.enqueue(28);
-        assertEquals(28, queueDynamicArray.peek());
+        queueDynamicArray.enqueue('e');
+        assertEquals('e', queueDynamicArray.peek());
+        queueDynamicArray.enqueue('q');
+        assertEquals('e', queueDynamicArray.peek());
+        queueDynamicArray.enqueue('w');
+        assertEquals('e', queueDynamicArray.peek());
+        queueDynamicArray.enqueue('f');
+        assertEquals('e', queueDynamicArray.peek());
+        queueDynamicArray.enqueue('o');
+        assertEquals('e', queueDynamicArray.peek());
     }
-
-
+    @Test
+    public void test_size(){
+        QueueDynamicArray queueDynamicArray = new QueueDynamicArray(5);
+        assertEquals(0, queueDynamicArray.size());
+        queueDynamicArray.enqueue('e');
+        assertEquals(1, queueDynamicArray.size());
+        queueDynamicArray.enqueue('a');
+        assertEquals(2, queueDynamicArray.size());
+        queueDynamicArray.enqueue('s');
+        assertEquals(3, queueDynamicArray.size());
+        queueDynamicArray.enqueue('d');
+        assertEquals(4, queueDynamicArray.size());
+        queueDynamicArray.enqueue('f');
+        assertEquals(5, queueDynamicArray.size());
+        queueDynamicArray.dequeue();
+        assertEquals(4, queueDynamicArray.size());
+        queueDynamicArray.dequeue();
+        assertEquals(3, queueDynamicArray.size());
+        queueDynamicArray.dequeue();
+        assertEquals(2, queueDynamicArray.size());
+        queueDynamicArray.dequeue();
+        assertEquals(1, queueDynamicArray.size());
+        queueDynamicArray.dequeue();
+        assertEquals(0, queueDynamicArray.size());
+    }
 
 }

3bfd365167debcf7a1d9d99ca865d0313b30be17
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-04-30 09:27:56 +0300

    start project

---
+++ AVL/src/code/Node.java
@@ -0,0 +1,23 @@
+package code;
+
+public class Node {
+    private Node parent, left, right;
+    private String key;
+    private int balance;
+
+
+    public Node(String key, Node parent) {
+        this.key = key;
+        this.parent = parent;
+        this.left = this.right = null;
+        this.balance = 0;
+    }
+
+    
+
+
+
+
+
+
+}

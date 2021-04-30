b59f217731e65eae9654e4e10ba035cd65e346fb
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-04-30 09:38:50 +0300

    maxmin

--- AVL/src/code/Tree.java
+++ AVL/src/code/Tree.java
@@ -6,4 +6,30 @@ public class Tree {
     public Tree() {
         root = null;
     }
+
+    public String getMin() {
+        if (root == null)
+            throw new RuntimeException("Min value isn't exist in empty tree");
+        return getMinRec(root).getKey();
+    }
+
+    public String getMax() {
+        if (root == null)
+            throw new RuntimeException("Max value isn't exist in empty tree");
+        return getMaxRec(root).getKey();
+    }
+
+    private static Node getMaxRec(Node root) {
+        if (root.getRight() != null)
+            return getMaxRec(root.getRight());
+        return root;
+    }
+
+    private static Node getMinRec(Node root) {
+        if (root.getLeft() != null)
+            return getMinRec(root.getLeft());
+        return root;
+    }
+
+    
 }

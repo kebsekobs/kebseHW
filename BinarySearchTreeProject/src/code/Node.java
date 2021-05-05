078fe5d9de531611ba85b5144f3cb82219678651
Sergey Kobyakov <47553277+kebsekobs@users.noreply.github.com>
2021-05-05 16:58:58 +0400

    rewrite node

--- BinarySearchTreeProject/src/code/Node.java
+++ BinarySearchTreeProject/src/code/Node.java
@@ -1,14 +1,46 @@
 package code;
 
 public class Node {
-        public Integer key;
-        public String value;
-        public Node left = null;
-        public Node right = null;
-        public Node parent = null;
+    private Node parent, left, right;
+    private String key;
 
-        public Node(Integer key, String value) {
-            this.key = key;
-            this.value = value;
-        }
+
+    public Node(String key, Node parent) {
+        this.key = key;
+        this.parent = parent;
+        this.left = this.right = null;
     }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setLeft(Node nLeft) {
+        this.left = nLeft;
+        if (nLeft != null)
+            nLeft.parent = this;
+    }
+
+    public void setRight(Node nRight) {
+        this.right = nRight;
+        if (nRight != null)
+            nRight.parent = this;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+
+
+
+
+}

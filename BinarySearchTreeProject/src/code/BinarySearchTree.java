c1c8d269d3b92d47b31838315bae73b946f950fe
Sergey Kobyakov <47553277+kebsekobs@users.noreply.github.com>
2021-05-05 18:25:36 +0400

    task 9 complete

--- BinarySearchTreeProject/src/code/BinarySearchTree.java
+++ BinarySearchTreeProject/src/code/BinarySearchTree.java
@@ -3,22 +3,40 @@ package code;
 public class BinarySearchTree {
     private Node root = null;
 
-    public Node search(Node node, String key){
+    public Node search(String key) {
+        if (root == null)
+            return null;
+        return searchRec(root, key);
+    }
+
+    private Node searchRec(Node node, String key){
             if (node == null | key.equals(node.getKey())){return node;}
-            if (key.compareTo(node.getKey()) < 0){return search(node.getLeft(), key);}
-            else{ return search(node.getRight(), key);}}
+            if (key.compareTo(node.getKey()) < 0){return searchRec(node.getLeft(), key);}
+            else{ return searchRec(node.getRight(), key);}}
 
-    public Node minimum(Node node){
+
+    public String minimum(){
+        if (root == null)
+            throw new RuntimeException("Min value isn't exist in empty tree");
+        return minimumRec(root).getKey();
+    }
+    private Node minimumRec(Node node){
             if (node.getLeft() == null){ return node;}
-            return minimum(node.getLeft());}
+            return minimumRec(node.getLeft());}
 
-    public Node maximum(Node node){
+    public String maximum() {
+        if (root == null)
+            throw new RuntimeException("Max value isn't exist in empty tree");
+        return maximumRec(root).getKey();
+    }
+
+    public Node maximumRec(Node node){
         if (node.getRight() == null){ return node;}
-        return maximum(node.getRight());}
+        return maximumRec(node.getRight());}
 
     public Node next(Node node){
             if(node.getRight() != null){
-                return minimum(node.getRight());}
+                return minimumRec(node.getRight());}
             Node anotherNode = node.getParent();
             while( anotherNode != null && node == anotherNode.getRight()){
                 node = anotherNode;
@@ -28,7 +46,7 @@ public class BinarySearchTree {
 
     public Node prev(Node node){
         if(node.getLeft() != null){
-            return maximum(node.getLeft());}
+            return maximumRec(node.getLeft());}
         Node anotherNode = node.getParent();
         while( anotherNode != null && node == anotherNode.getLeft()){
             node = anotherNode;
@@ -74,31 +92,32 @@ public class BinarySearchTree {
 
 
     public Node deleteRec(Node node, String key){
-    Node parent = node.getParent();
-        if (node.getLeft() == null && node.getRight() == null){
-            if (parent.getLeft() == node){parent.setLeft(null);}
-            if (parent.getRight() == node){ parent.setRight(null);}}
-        else if (node.getLeft() == null | node.getRight() == null){
-            if (node.getLeft() == null){
-                if (parent.getLeft() == node){parent.setLeft(node.getRight());}
-                else{parent.setRight(node.getRight());}
-                node.getRight().setParent(parent);}
-       else{
-           if(parent.getLeft() == node){ parent.setLeft(node.getLeft());}
-           else{ parent.setRight(node.getLeft());}
-           node.getLeft().setParent(parent);}}
-       else{
-            Node successor = next(node);
-            node.setKey(successor.getKey());
-            if (successor.getParent().getLeft() == successor){
-                successor.getParent().setLeft(successor.getRight());
-                if (successor.getRight() != null){
-                    successor.getRight().setParent(successor.getParent());}}
-            else{
-                successor.getParent().setRight(successor.getRight());
-                if (successor.getRight() != null){
-                    successor.getRight().setParent(successor.getParent());}}}
-       return parent;
+        if (node == null) {
+            return null;
+        }
+        if (key.equals(node.getKey())) {
+            if (node.getLeft() == null && node.getRight() == null) {
+                return null;
+            }
+            if (node.getRight() == null) {
+                return node.getLeft();
+            }
+
+            if (node.getLeft() == null) {
+                return node.getRight();
+            }
+            String smallestValue = minimumRec(node.getRight()).getKey();
+            node.setKey(smallestValue);
+            node.setRight(deleteRec(node.getRight(), smallestValue));
+            return node;
+        }
+        if (key.compareTo(node.getKey()) < 0) {
+            node.setLeft(deleteRec(node.getLeft(), key));
+            return node;
+        }
+
+        node.setRight(deleteRec(node.getRight(), key));
+            return node;
     }
 }
 

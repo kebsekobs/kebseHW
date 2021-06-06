faaa8ea43d0e97e92f19df91bd9aad911182c024
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-06-06 13:25:08 +0300

    add

--- ListHashTable/src/code/ListHashTable.java
+++ ListHashTable/src/code/ListHashTable.java
@@ -13,4 +13,22 @@ public class ListHashTable<K, T> {
             data.add(null);
         }
     }
+
+    public T add(K key, T value) {
+        DoubleLinkedListElement<K, T> node = getNodeForKey(key);
+        if (node != null) {
+            T oldValue = node.value;
+            node.value = value;
+            return oldValue;
+        }
+
+        node = new DoubleLinkedListElement<K, T>(null, null, value, key);
+        int index = getIndexForKey(key);
+        if (data.get(index) != null) {
+            node.next = data.get(index);
+            node.next.prev = node;
+        }
+        data.set(index, node);
+        return null;
+    }
 }

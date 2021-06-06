6ec9455b5161d8d2b8e7d734710c4dae8d056b0b
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-06-06 13:23:48 +0300

    (0_0)

--- ListHashTable/src/code/DoubleLinkedListElement.java
+++ ListHashTable/src/code/DoubleLinkedListElement.java
@@ -1,13 +1,16 @@
 package code;
 
-public class DoubleLinkedListElement <T> {
-    DoubleLinkedListElement<T> next;
-    DoubleLinkedListElement<T> prev;
+public class DoubleLinkedListElement <K, T> {
+    DoubleLinkedListElement<K, T> next;
+    DoubleLinkedListElement<K, T> prev;
+    K key;
     T value;
 
-    public DoubleLinkedListElement(DoubleLinkedListElement Prev, DoubleLinkedListElement Next, T Value) {
+
+    public DoubleLinkedListElement(DoubleLinkedListElement Prev, DoubleLinkedListElement Next, T Value, K Key) {
         prev = Prev;
         next = Next;
         value = Value;
+        key = Key;
     }
 }

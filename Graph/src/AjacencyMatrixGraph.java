571b50eb3d67ae99b1cb671a5e8ba5e143819aae
kebsekobs <47553277+kebsekobs@users.noreply.github.com>
2021-06-07 19:26:23 +0300

    zabyl pro commity =( no tut vse ravno vse super prosto(simple)

--- Graph/src/AjacencyMatrixGraph.java
+++ Graph/src/AjacencyMatrixGraph.java
@@ -6,4 +6,32 @@ public class AjacencyMatrixGraph {
         this.vertex = vertex;
         matrix = new int[vertex][vertex];
     }
+
+    public void addEdge(int start, int finish, int weight){
+        matrix[start][finish] = weight;
+    }
+
+    public int getEdge(int start, int finish){
+        return matrix[start][finish];
+    }
+
+    public void deleteEdge(int start, int finish){
+        matrix[start][finish] = 0;
+    }
+
+    public void addVertex(){
+        vertex++;
+        int[][] newMatrix = new int[vertex][vertex];
+        for (int i = 0; i < vertex; i++){
+            for (int j = 0; j < vertex; j++){
+                newMatrix[i][j] = matrix[i][j];
+            }
+        }
+        matrix = newMatrix;
+    }
+
+    public void cleanUp(){
+        matrix = new int[0][0];
+    }
+
 }
